60a756284226922143244524d571e8621704828a
Your Name <you@example.com>
2022-10-28 19:37:18 -0400

    Java completed, but cone option not functional

--- index.js
+++ index.js
@@ -1,53 +1,80 @@
 "use strict";
 
-window.onload = init; 
+window.onload = function() {
+document.getElementById("options").style.display = "none";
+document.getElementById("cupCheck").onclick = function()
 
-function init() {
-    // need to find the button element
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.onclick = onSubmitBtnClicked;
+{
+document.getElementById("options").style.display = "block";
 }
 
-//define the submit button function 
-function onSubmitBtnClicked() {
-    //find variables and initialize
+    document.getElementById("coneCheck").onclick = function()
+{
+    document.getElementById("options").style.display = "none";
+}
+
+document.getElementById("submitBtn").onclick = totalCost;
+}
+
+
+
+function totalCost() {
+let numOfScoopsInput = document.getElementById("numOfScoopsInput");
+let newScoop = numOfScoopsInput.value;
 let oneScoop = 2.25;
-let numberOfScoop;
-let totalScoop = (numberofScoop * 1.25) + oneScoop;
-let sprinkles = .50;
-let whipCream = .25;
-let hotFudge = 1.25;
-let cherry = .25;
+//extra scoop 1.25;
+let totalScoop = ((newScoop -1 )* 1.25) + oneScoop;
+let sprinklesCost = 0;
+let whipCreamCost = 0;
+let hotFudgeCost = 0;
+let cherryCost = 0;
 
+let sprinkles = document.getElementById("sprinklesCheckbox").checked;
+if (sprinkles) {
+    sprinklesCost= .50;
+} 
+else {
+    sprinklesCost = 0;
+}
 
-
-//only cups can have toppings 
-const sprinklesCheckbox = document.getElementById(" sprinklesCheckbox");
-const whipCreamCheckbox = document.getElementById("whipCreamCheckbox");
-const hotFudgeCheckbox = document.getElementById("hotFudgeCheckbox");
-const cherryCheckbox = document.getElementById("cherryCheckbox");
-
-
-//cups pricing 
-
-
-
-
-//cone pricing 
-let cone = document.getElementById("coneCheck").checked;
-const cone = document.getElementById("coneCheck");
-if (cone) {
-    cone = (2.25);
+let whipCream = document.getElementById("whipCreamCheckbox").checked;
+if (whipCream) {
+    whipCreamCost = .25;
 }
 else {
-    cone = (totalScoop);
+    whipCreamCost = 0;
+}
+
+let hotFudge = document.getElementById("hotFudgeCheckbox").checked;
+if (hotFudge) {
+    hotFudgeCost = 1.25;
+}
+else { 
+  hotFudgeCost = 0;
 }
 
 
+let cherry = document.getElementById("cherryCheckbox").checked;
+if (cherry) {
+    cherryCost = .25;
+}
+else {
+    cherryCost = 0;
+}
+
+let basePrice = document.getElementById("basePrice");
+let add = totalScoop + sprinklesCost + whipCreamCost + hotFudgeCost + cherryCost;
+
+basePrice.innerHTML = " Base price: $ " + add;
+
+let tax = 0.08 * add;
+let taxTotal = document.getElementById("tax");
+taxTotal.innerHTML =  " Tax: $ " + tax;
+
+let totalCost = add + tax;
+let total = document.getElementById("totalDue");
+total.innerHTML = " Total due: $ " + totalCost;
 
 
-
-
-return onSubmitBtnClicked();
 
 }
